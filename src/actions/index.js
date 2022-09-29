8d8e8b99ecfc7834224b2e41198296869a8875dc
artemBodryi <artembodry@gmail.com>
2022-09-29 15:07:02 +0300

    -had added logic in "+", "-" and "delete" buttons - had made refactoring in reducer (initialState was divided on 2 objects) -had moved updateBookList and updateShoppingCart funcs into own files in reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,8 +22,23 @@ export const bookAddedToCart = (bookId) => {
   return {
     type: 'BOOK_ADDED_TO_CART',
     payload: bookId
-  }
-}
+  };
+};
+
+export const bookRemovedFromCart = (bookId) => {
+  return {
+    type: 'BOOK_REMOVED_FROM_CART',
+    payload: bookId
+  };
+};
+
+export const allBooksRemovedFromCart = (bookId) => {
+  return {
+    type: 'ALL_BOOKS_REMOVED_FROM_CART',
+    payload: bookId
+  };
+};
+
 
 const fetchBooks = (bookstoreService, dispatch) => () => {
   dispatch(booksRequested());
--- src/components/shopping-cart-table/shopping-cart-table.js
+++ src/components/shopping-cart-table/shopping-cart-table.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./shopping-cart-table.css";
 import { connect } from "react-redux";
+import {
+  bookAddedToCart,
+  bookRemovedFromCart,
+  allBooksRemovedFromCart,
+} from "../../actions/index";
 
 const ShoppingCartTable = ({
   items,
@@ -63,27 +68,17 @@ const ShoppingCartTable = ({
   );
 };
 
-const mapStateToProps = ({ cartItems, orderTotal }) => {
+const mapStateToProps = ({ shoppingCart: {cartItems, orderTotal} }) => {
   return {
     items: cartItems,
     total: orderTotal,
   };
 };
 
-const mapDispatchToProps = () => {
-  return {
-    onIncrease: (id) => {
-      console.log(`Increase ${id}`);
-    },
-
-    onDecrease: (id) => {
-      console.log(`Decrease ${id}`);
-    },
-
-    onDelete: (id) => {
-      console.log(`Delete ${id}`);
-    },
-  };
+const mapDispatchToProps = {
+  onIncrease: bookAddedToCart,
+  onDecrease: bookRemovedFromCart,
+  onDelete: allBooksRemovedFromCart
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCartTable);
